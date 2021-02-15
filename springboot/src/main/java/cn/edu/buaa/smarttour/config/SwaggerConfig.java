2490a4278eb928cb9b9cfc6b6702c15e35a7f58c
h56983577 <h56983577@126.com>
2021-02-15 21:58:34 +0800

    init_添加swaggerUI

---
+++ springboot/src/main/java/cn/edu/buaa/smarttour/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package cn.edu.buaa.smarttour.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket customDocket(){
+        ParameterBuilder ticketPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        ticketPar.name("authorization").description("jwt")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).build(); //header中的ticket参数非必填，传空也可以
+        pars.add(ticketPar.build());    //根据每个方法名也知道当前方法在设置什么参数
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .build()
+                .globalOperationParameters(pars)
+                .apiInfo(apiInfo());
+    }
+
+    //基本信息，页面展示
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("智慧景区-ARTour")
+                .description("后端接口文档")
+                //联系人实体类
+                .contact(
+                        new Contact("Muyung", "https://github.com/h56983577", "h56983577@buaa.edu.cn")
+                )
+                //版本号
+                .version("1.0.0-SNAPSHOT")
+                .build();
+    }
+}
+
