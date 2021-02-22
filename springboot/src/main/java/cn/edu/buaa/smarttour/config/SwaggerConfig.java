9d856a9b794591350c33c1712c3dbd4e1ccbfdd2
h56983577 <h56983577@126.com>
2021-02-22 15:39:34 +0800

    feature_授权与鉴权

--- springboot/src/main/java/cn/edu/buaa/smarttour/config/SwaggerConfig.java
+++ springboot/src/main/java/cn/edu/buaa/smarttour/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
@@ -33,6 +34,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("(?!/error.*).*"))
                 .build()
                 .globalOperationParameters(pars)
                 .apiInfo(apiInfo());
